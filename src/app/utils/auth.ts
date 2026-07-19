b29369776bee53ab99ff06af0b76d5f5b708a443
Md. Sakib <mohammadsakib7679@gmail.com>
2026-07-19 23:17:38 +0600

    write hash fn for password hash

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { ApiError } from "../error/ApiError";
-import { verifyAccessToken } from "./jwt";
+import { verifyAccessToken } from "../utils/jwt";
 
 export const authenticate = (
   req: Request,
