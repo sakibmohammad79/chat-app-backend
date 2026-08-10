0c967737a0cdaf7774fa61e8d06d8df7c0c66f81
Md. Sakib <mohammadsakib7679@gmail.com>
2026-08-10 22:29:32 +0600

    write zod validation for get message query

--- src/app/module/message/message.validation.ts
+++ src/app/module/message/message.validation.ts
@@ -15,4 +15,16 @@ export const sendMessageSchema = z.object({
   }),
 });
 
+export const getMessagesSchema = z.object({
+  params: z.object({
+    id: z.uuid("Invalid conversation ID"),
+  }),
+  query: z.object({
+    //cursor = last fetched message id, get cursor id's before messages
+    cursor: z.uuid().optional(),
+    limit: z.coerce.number().int().positive().max(50).default(30),
+  }),
+});
+
 export type SendMessageInput = z.infer<typeof sendMessageSchema>["body"];
+export type getMessageQuery = z.infer<typeof getMessagesSchema>["query"];
