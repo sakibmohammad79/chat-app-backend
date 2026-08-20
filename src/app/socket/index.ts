2121cbf1576a5e52381ba4752022649c5dc8468e
Md. Sakib <mohammadsakib7679@gmail.com>
2026-08-20 22:17:42 +0600

    authenticate token every time using io

--- src/app/socket/index.ts
+++ src/app/socket/index.ts
@@ -1,6 +1,9 @@
 import { Server as HttpServer } from "http";
 import { Server } from "socket.io";
 import { config } from "../config";
+import { setIO } from "./socket.instance";
+import { verifyAccessToken } from "../utils/jwt";
+import type { AuthSocket } from "./socket.types";
 
 export const initSocket = (httpServer: HttpServer) => {
   const io = new Server(httpServer, {
@@ -12,5 +15,32 @@ export const initSocket = (httpServer: HttpServer) => {
     connectionStateRecovery: {
       maxDisconnectionDuration: 2 * 60 * 1000, //2min
     },
+  });
+  setIO(io);
+
+  io.use((socket, next) => {
+    try {
+      // Client send token in to way
+      // socket.auth = { token: "..." }  ← recommended
+      // query: ?token=...               ← fallback
+      const token =
+        socket.handshake.auth?.token ||
+        (socket.handshake.query?.token as string);
+
+      if (!token) {
+        return next(new Error("Authentication token missing"));
+      }
+
+      const decoded = verifyAccessToken(token);
+      //user attach in socket
+      (socket as AuthSocket).user = {
+        id: decoded.id,
+        email: decoded.email,
+      };
+
+      next();
+    } catch {
+      next(new Error("Invalid or expired token"));
+    }
   });
 };
