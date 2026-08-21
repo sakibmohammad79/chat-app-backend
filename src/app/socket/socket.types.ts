6d075a48fbd0e5c6ffe74fcdf3c4af170359f240
Md. Sakib <mohammadsakib7679@gmail.com>
2026-08-21 23:56:51 +0600

    write client to server event interface

--- src/app/socket/socket.types.ts
+++ src/app/socket/socket.types.ts
@@ -6,3 +6,21 @@ export interface AuthSocket extends Socket {
     email: string;
   };
 }
+
+//client -> server events
+export interface ClientToServerEvents {
+  join_conversation: (conversationId: string) => void;
+  leave_conversation: (conversationId: string) => void;
+
+  send_message: (data: {
+    conversationId: string;
+    content: string;
+    type?: "TEXT" | "IMAGE" | "FILE";
+    replyToId?: string;
+  }) => void;
+
+  typing_start: (conversationId: string) => void;
+  typing_stop: (conversationId: string) => void;
+
+  mark_seen: (data: { conversationId: string; messageId: string }) => void;
+}
