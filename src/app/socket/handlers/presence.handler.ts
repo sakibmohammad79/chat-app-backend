183d61346fa11ed20949f018c27f5f418e7656ce
Md. Sakib <mohammadsakib7679@gmail.com>
2026-08-24 22:37:50 +0600

    write register presence handler

---
+++ src/app/socket/handlers/presence.handler.ts
@@ -0,0 +1,63 @@
+
+import { prisma } from "../../../lib/prisma";
+import { getIO } from "../socket.instance";
+import type { AuthSocket } from "../socket.types";
+
+export const registerPresenceHandlers = (socket: AuthSocket) => {
+  const userId = socket.user.id;
+  const io = getIO();
+
+  //  User Online 
+  // online mark when connected
+  const handleOnline = async () => {
+    try {
+      await prisma.user.update({
+        where: { id: userId },
+        data: { isOnline: true },
+      });
+
+   
+      const memberships = await prisma.conversationMember.findMany({
+        where: { userId },
+        select: { conversationId: true },
+      });
+
+      memberships.forEach(({ conversationId }) => {
+        socket.to(conversationId).emit("user_online", userId);
+      });
+    } catch (err) {
+      console.error("Presence online error:", err);
+    }
+  };
+
+  // ─── User Offline 
+  // offline mark when user disconnected
+  const handleOffline = async () => {
+    try {
+      const lastSeen = new Date();
+
+      await prisma.user.update({
+        where: { id: userId },
+        data: { isOnline: false, lastSeen },
+      });
+
+    
+      const memberships = await prisma.conversationMember.findMany({
+        where: { userId },
+        select: { conversationId: true },
+      });
+
+      memberships.forEach(({ conversationId }) => {
+        socket.to(conversationId).emit("user_offline", { userId, lastSeen });
+      });
+    } catch (err) {
+      console.error("Presence offline error:", err);
+    }
+  };
+
+  //online
+  handleOnline();
+
+  // offline
+  socket.on("disconnect", handleOffline);
+};
